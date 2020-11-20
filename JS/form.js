8654e8f76acb1325017c5cc9ad0ec3da7c06e553
lalit-003 <gahlawatl7@gmail.com>
2020-11-20 07:57:00 +0530

    Added functionality to add An Contact to ContactList and LocalStorage on clicking add button

--- JS/form.js
+++ JS/form.js
@@ -22,7 +22,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
                     return;
                 }
                 try {
-                    (new AddressBook()).phoneNumber = phone.value;
+                    (new AddressBook())._phoneNumber = phone.value;
                     phoneError.textContent = "";
                 } catch (e) {
                     phoneError.textContent = e;
@@ -37,7 +37,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
                         return;
                     }
                     try {
-                        (new AddressBook()).address = address.value;
+                        (new AddressBook())._address = address.value;
                         addressError.textContent = "";
                     } catch (e) {
                         addressError.textContent = e;
@@ -46,4 +46,66 @@ window.addEventListener('DOMContentLoaded', (event) => {
         
     });
 
+    //on submit
+
+    const save = () => {
+        try{
+            let addressBookData = createAddressBook();
+            createAndUpdateStorage(addressBookData);
+        }catch(e){
+            return;
+        }
+    }
     
+    const createAddressBook = ()=>{
+        let addressBookData = new AddressBook();
+        addressBookData._id = new Date().getTime();
+        try{
+          //  console.log("adding name");
+            addressBookData._name=document.querySelector('#name').value;
+          //  console.log("added name");
+        }catch(e){
+            setTextValue('.text-error', e)
+            throw e;
+        }
+        console.log(document.querySelector('#name').value);
+        addressBookData._phoneNumber=document.querySelector('#phone').value;
+        console.log("added phone");
+        addressBookData._address=document.querySelector('#address').value;
+        addressBookData._city=document.querySelector('#city').value;
+        addressBookData._state=document.querySelector('#state').value;
+        addressBookData._zip=document.querySelector('#zip').value;
+        alert(addressBookData.toString());
+        return addressBookData;
+    }
+
+    function createAndUpdateStorage (addressBookData){
+        let contactList = JSON.parse(localStorage.getItem("ContactList"));
+        if(contactList!=undefined){
+            contactList.push(addressBookData);
+        }else{
+            contactList=[addressBookData];
+        }
+        alert(contactList.toString());
+        localStorage.setItem("ContactList",JSON.stringify(contactList));
+    }
+    
+    const getSelectedValues=(propertyValue)=>{
+        let allItems = document.querySelectorAll(propertyValue)
+        let selItems = [] ;
+        allItems.forEach(item=>{
+            if(item.checked)
+            selItems.push(item.value);
+        })
+        return selItems;
+    }
+    
+    const getInputValueById = (id) => {
+        let value = document.querySelector(id).value;
+        return value;
+    }
+
+    const getInputElementValue = (id) => {
+        let value = document.getElementById(id).value
+        return value
+    }  
